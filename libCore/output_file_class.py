2b1dab3290abcb38d21b2145498fb4ce4a14e013
LittleViewer <planete.adoecrivain@gmail.com>
2026-03-02 08:29:45 +0100

    Add unauthorized-account list and checks
    Introduce un_authorized_account.json to list blocked accounts and add pipe_un_authorized_account() in libCore/output_file_class.py to load account names. main.py now loads the set of unauthorized accounts and prevents conversion for those accounts, emitting a warning and copying it to the clipboard; normal conversion and logging remain unchanged for other accounts. save_output.txt was updated with an additional saved entry.

--- libCore/output_file_class.py
+++ libCore/output_file_class.py
@@ -1,4 +1,6 @@
 import libCore.util_class as luC
+import json
+
 class output_file:
 
     def pipe_output(self, content, link="..\save_output.txt"):
@@ -11,6 +13,16 @@ class output_file:
         content = handle.read()
         handle.close()
         return content
+
+    def pipe_un_authorized_account(self, link="..\\un_authorized_account.json"):
+        handle = self.luC_.file_open(self.luC_.absolute_link(link),"r")
+        content = handle.read()
+        list_account = []
+        for one_account in json.loads(content):
+            list_account.append(one_account["name_account"])
+        handle.close()
+        return list_account
+    
 
     def __init__(self):
         self.luC_ = luC.util()
--- main.py
+++ main.py
@@ -9,26 +9,31 @@ ofC_ = ofC.output_file()
 
 code_country = "fr"
 is_clic = False 
-
+list_un_authorized_account = set(ofC_.pipe_un_authorized_account())
 while True:
+        
         event = keyboard.read_hotkey(suppress=False)
 
         if event == "ctrl+x" :
             copy = pyperclip.paste()
             value = copy.split("x.com")
-            
-            try:
-                new_formated_link = value[0]+"fxtwitter.com"+value[1]+f"/{code_country}"
-                print(new_formated_link)
-
-                copy_list = copy.split("/")
-                date_hours_minute = str(datetime.datetime.now()).split('.')
-                pyperclip.copy(f"[ {date_hours_minute[0]} - [{copy_list[3]}](<https://x.com/{copy_list[3]}>) - {new_formated_link} ]")
+            copy_list = copy.split("/")
+            if copy_list[3] in list_un_authorized_account:
+                warn = "You have just attempted to convert a link from an unauthorized account. If this account needs to be authorized, please refer to the file: un_authorized_account.json"
+                luC_.error_with_reason(warn)
+                pyperclip.copy(warn)
+            else:
+                try:
+                    new_formated_link = value[0]+"fxtwitter.com"+value[1]+f"/{code_country}"
+                    print(new_formated_link)
+               
+                    date_hours_minute = str(datetime.datetime.now()).split('.')
+                    pyperclip.copy(f"[ {date_hours_minute[0]} - [{copy_list[3]}](<https://x.com/{copy_list[3]}>) - {new_formated_link} ]")
                 
-                output_save =  {"date_time" :str(datetime.datetime.now()), "account": copy_list[3], "link_account" : f"https://x.com/{copy_list[3]}", "id_post" : copy_list[5], "original_link" : copy, "new_formated_link" : new_formated_link}
-                ofC_.pipe_output(output_save)
-            except:
-                luC_.error_with_reason("Not good link format")
+                    output_save =  {"date_time" :str(datetime.datetime.now()), "account": copy_list[3], "link_account" : f"https://x.com/{copy_list[3]}", "id_post" : copy_list[5], "original_link" : copy, "new_formated_link" : new_formated_link}
+                    ofC_.pipe_output(output_save)
+                except:
+                    luC_.error_with_reason("Not good link format")
 
         elif event == "ctrl+h":
              pyperclip.copy(ofC_.pipe_copy_history())
